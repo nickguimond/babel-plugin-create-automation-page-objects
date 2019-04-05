575cab0f2af3297ee73d59e401f60f210f50c260
Nick Guimond <nguimond@serenova.com>
2019-04-05 11:13:48 -0300

    json instead of js

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
 const fs = require('fs');
 
 const createPageObjects = function() {
-    const pageObjects = {};
+    const pageObjectsUnordered = {};
     return {
       visitor: {
         JSXElement: {
           enter(path, state) {
             path.node.openingElement.attributes.forEach(att => {
-              if(att.name && att.name.name === 'automation') {
-                  pageObjects[att.value.value] = att.value.value;
-                  let fileData = state.opts.filePrefix;
 
-                  Object.keys(pageObjects).forEach(elem => {
-                      fileData += state.opts.elementTemplate.replace(/ELEMENT/g, elem);
+              if(att.name && att.name.name === 'data-automation') {
+                pageObjectsUnordered[att.value.value] = att.value.value;
+                  const pageObjectsOrdered = {};
+                  Object.keys(pageObjectsUnordered).sort().forEach(function(key) {
+                    pageObjectsOrdered[key] = pageObjectsUnordered[key];
                   });
-
-                  fileData += state.opts.fileSuffix;
-                  fs.writeFileSync(state.opts.saveFilePath, fileData, function (err) {
+                  fs.writeFileSync(state.opts.saveFilePath, JSON.stringify(pageObjectsOrdered), 'utf8' , function (err) {
                     if (err) return;
                   });
               }
+
             })
           }
         }
